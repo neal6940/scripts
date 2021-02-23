a948b8a07a166e474a94a6ad11d222fdfb258ead
Neal Dreher <62036221+neal6940@users.noreply.github.com>
2021-02-22 21:33:12 -0600

    Update ec2_sg_rules.py

--- ec2_sg_rules.py
+++ ec2_sg_rules.py
@@ -12,9 +12,19 @@
 # - added acct_num input from cli arg and the acct_num and region to the output
 ######################################################################################################################
 # BASH SCRIPT:
-
-
-
+# #!/bin/bash
+# # use the faws toolbox to grab the aws accounts for the customer
+# aws_accounts=$(faws account list-accounts -r 1122393 |grep 'AWS Account'|awk '{print $3}')
+# # print the csv headers to the output file
+# echo "\"Account\"\,\"Region\"\,\"Group-Name\"\,\"Group-ID\"\,\"In\/Out\"\,\"Protocol\"\,\"Port\"\,\"Source/Destination\"" > ec2_sg_rules.csv
+# # parse through each aws account and output the python results to a csv
+# for aws_account in ${aws_accounts[@]}
+# do
+#     echo $aws_account
+#     # log into the specific account
+#     DDI=1122393; ACCT=$aws_account; faws -r $DDI env -a $ACCT && eval "$(faws -r $DDI env -a $ACCT)"
+#     python3 ./ec2_sg_rules.py $aws_account >> ec2_sg_rules.csv
+# done
 ######################################################################################################################
 
 from __future__ import print_function
